e1f7b0c96435d40b5da9dcf0c0a6205efdef9c5b
Muhammad Talha Khan <talhakhan325@gmail.com>
2023-10-13 11:22:38 +0500

    Update firebase.js

--- Add Code Here/Javascript/YouTube_Clone/src/firebase.js
+++ Add Code Here/Javascript/YouTube_Clone/src/firebase.js
@@ -1,6 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAP29MHJW-ToTUgxZ8qQWKEwsFVdaFCO8Q",
+    apiKey: "Add your API key!",
     authDomain: "clone-9ac35.firebaseapp.com",
     projectId: "clone-9ac35",
     storageBucket: "clone-9ac35.appspot.com",
