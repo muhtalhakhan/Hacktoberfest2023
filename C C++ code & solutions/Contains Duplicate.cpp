476bcb6543db213029b42ef577e9d952a78438ca
CHIRANJIB MOHAPATRA <77581863+chiranjib2001@users.noreply.github.com>
2023-10-13 10:43:08 +0530

    Create Contains Duplicate.cpp

---
+++ C C++ code & solutions/Contains Duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {  
+    public boolean containsDuplicate(int[] nums)
+   {
+        HashSet<Integer> s =new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(s.contains(nums[i]))
+                return true;
+            else s.add(nums[i]);
+        }return false;
+    } 
+    
+}
