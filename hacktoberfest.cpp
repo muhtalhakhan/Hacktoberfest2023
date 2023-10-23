0d2ae2001fc915b4d1eaeb54a0a6d069bcbb3c02
SulagnaKundu <sulagnakundu7@gmail.com>
2023-10-22 18:50:40 -0700

    Changes

---
+++ hacktoberfest.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string name = "SulagnaKundu";
+    string learning = "OpenSource";
+    cout<<"Name : "<<name<<"\n"<<"Learning : "<<learning<<"\n";
+    return 0;
+}
+
