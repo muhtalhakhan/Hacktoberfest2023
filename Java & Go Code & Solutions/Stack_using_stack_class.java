3eb84a96ad5017decb5f54072ce433a819ce0b78
Mohammad Sahil <144834170+mdsahilnoob@users.noreply.github.com>
2023-10-30 08:17:47 +0530

    Create Stack_using_stack_class.java

---
+++ Java & Go Code & Solutions/Stack_using_stack_class.java
@@ -0,0 +1,20 @@
+//Implement stack using Stack class
+import java.util.Stack;
+
+class Main {
+  public static void main(String[] args) {
+
+    // create an object of Stack class
+    Stack<String> animals= new Stack<>();
+
+    // push elements to top of stack
+    animals.push("Dog");
+    animals.push("Horse");
+    animals.push("Cat");
+    System.out.println("Stack: " + animals);
+
+    // pop element from top of stack
+    animals.pop();
+    System.out.println("Stack after pop: " + animals);
+    }
+}
